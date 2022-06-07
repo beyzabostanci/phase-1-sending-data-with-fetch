824bd83e0feb65c93ff406173efb868cec28f883
beyzabostanci <beyza_bostanc@hotmail.com>
2022-06-07 12:17:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    const user ={
+        "name": userName,
+        "email": userEmail
+    }//our user needs
+    const userData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(user),
+      };
+
+    return fetch("http://localhost:3000/users", userData)//fetching data and giving it an order
+    .then(function (response) {
+        return response.json();//give a response
+      })
+      .then(function (object) {
+        console.log(object);
+        document.body.innerHTML = object.id//show them in the html body
+      })
+      .catch(function (error) {
+        console.log(error.message);
+        document.body.innerHTML = error.message//error message
+      })
+}
